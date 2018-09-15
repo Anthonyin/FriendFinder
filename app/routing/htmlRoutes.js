94c106d47e1a35e0799e4f7b25e8fd3151b1883f
Anthony Yin <anthony923x@gmail.com>
2018-09-14 22:02:12 -0400

    test

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,10 +1,10 @@
 var path = require('path');
 
 module.exports = function(app) {
-  app.get('/survey', function(req, res) {
-    res.sendFile(path.join(__dirname + '/../public/survey.html'));
+  app.get('/survey.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/../public/home.html'));
   });
   app.use(function(req, res) {
-    res.sendFile(path.join(__dirname + '/../public/home.html'));
+    res.sendFile(path.join(__dirname + '/../public/survey.html'));
   });
 };
