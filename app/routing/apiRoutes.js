5fed71e96de128ddaffaeab2476bb5e4250b638a
Anthony Yin <anthony923x@gmail.com>
2018-09-14 21:38:14 -0400

    fixed bug

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,15 +1,16 @@
-var friends = reuqire('../data/friends.js');
+var path = require('path');
+var friends = require(path.join(__dirname, '../data/friends.js'));
 
 module.exports = function(app) {
   app.get('/api/friedns', function(req, res) {
     res.json(friends);
   });
 
-  app.post('/api/friends',function(req,res){
-    var bestMatch ={
-      name="",
-      photo="",
-      friendDifference=1000
+  app.post('/api/friends', function(req, res) {
+    var bestMatch = {
+      name: '',
+      photo: '',
+      friendDifference: 1000
     };
     var userData = req.body;
     var userName = userData.name;
@@ -18,20 +19,22 @@ module.exports = function(app) {
 
     var totalDifference = 0;
 
-    for(var i=0;i<friends.length;i++){
-      console.log(friends[i].name)
+    for (var i = 0; i < friends.length; i++) {
+      console.log(friends[i].name);
       totalDifference = 0;
 
-      for(var a=0;a<friends[i].socres[a];a++){
-        totalDifference+=Marth.abs(parseInt(userScores[a]-parseInt(friends[i].scores[a])));
-        if(totalDifference<=bestMatch.friendDifference){
-          bestMatch.name=friends[i].name;
-          bestMatch.photo=friends[i].photo;
-          bestMatch.friendDifference=totalDifference;
+      for (var a = 0; a < friends[i].socres[a]; a++) {
+        totalDifference += Marth.abs(
+          parseInt(userScores[a] - parseInt(friends[i].scores[a]))
+        );
+        if (totalDifference <= bestMatch.friendDifference) {
+          bestMatch.name = friends[i].name;
+          bestMatch.photo = friends[i].photo;
+          bestMatch.friendDifference = totalDifference;
         }
       }
     }
     friends.push(userData);
     res.json(bestMatch);
-  })
+  });
 };
--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-require('./app/routing/apiRoutes.js')(app);
-require('./app/routing/htmlRoutes.js')(app);
+require(path.join(__dirname,'./app/routing/apiRoutes.js'))(app);
+require(path.join(__dirname,'./app/routing/htmlRoutes.js'))(app);
 
 app.listen(PORT, function() {
   console.log('App listening on PORT:' + PORT);
